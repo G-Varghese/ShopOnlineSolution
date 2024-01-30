2c9eb05b0974043130bbbaed11fe2998544a6b14
vargheg3 <geethuvarghese14@gmail.com>
2024-01-31 01:24:12 +1300

    localhost
    Change in the localhost address

--- OnlineStore.Api/Program.cs
+++ OnlineStore.Api/Program.cs
@@ -4,48 +4,46 @@ using ShopOnline.Api.Data;
 using ShopOnline.Api.Repositories.Contracts;
 
 
-internal class Program
-{
-	private static void Main(string[] args)
-	{
-		var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 		// Add services to the container.
 
-		builder.Services.AddControllers();
+builder.Services.AddControllers();
 		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-		builder.Services.AddEndpointsApiExplorer();
-		builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 		//***IMPORTANT INSTRUCTION HERE - MUST CONFIGURE CONNECTION BEFORE RUNNING MIGRATIONS
-		builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
 			options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
 		);
 
-		builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
-		var app = builder.Build();
+var app = builder.Build();
 
-		// Configure the HTTP request pipeline.
-		app.UseHttpsRedirection();
 
-		app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 		
 
-		app.MapControllers();
+app.MapControllers();
 
-		if (app.Environment.IsDevelopment())
-		{
-			app.UseSwagger();
-			app.UseSwaggerUI();
-		}
-		app.UseCors(policy =>
+
+		
+app.UseCors(policy =>
 			policy.WithOrigins("http://localhost:7122", "https://localhost:7122")
 			.AllowAnyMethod()
 			.WithHeaders(HeaderNames.ContentType)
 		);
 
 		app.Run();
-	}
-}
